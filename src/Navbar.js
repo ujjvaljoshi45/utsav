8c023d7e798cd9ce0a0c3cd6945aa4d843c964e6
Ujjval Joshi <ujjvaljoshi45@gmail.com>
2023-07-01 09:32:36 +0530

    light

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand text-light" href="">Utsav</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
